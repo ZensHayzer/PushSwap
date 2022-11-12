8d5a559d2d33eb33fa8c7bd35f5b10f3421b81b9
ajeanne <ajeanne@student.42.fr>
2022-11-12 19:38:11 +0100

    THIS IS THE END

---
+++ bonus/moving_func1.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moving_func1.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajeanne <ajeanne@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 17:01:06 by ajeanne           #+#    #+#             */
+/*   Updated: 2022/11/12 17:01:36 by ajeanne          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+int mv_rrotate(t_list_data **lst)
+{
+	if (*lst && (*lst)->next)
+	{
+		lst_rrotate(lst);
+		while ((*lst)->prev)
+			*lst = (*lst)->prev;
+		return (0);
+	}
+	return (1);
+}
+
+int double_mv_rrotate(t_list_data **lsta, t_list_data **lstb)
+{
+	if (!mv_rrotate(lsta))
+		if (!mv_rrotate(lstb))
+			return (0);
+	return (1);
+}
